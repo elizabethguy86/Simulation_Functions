69f8a218c566b0506d1d9148592e85373bfc83a9
Elizabeth Glenn Guy <elizg23@yahoo.com>
2018-12-20 11:48:08 -0800

    modify star_cards function to new recursion function

--- Simulations.py
+++ Simulations.py
@@ -32,33 +32,29 @@ probability_A = sum(coin_flip() for _ in range(1000000))
 probability_A/1000000
 
 
-def check_for_diamonds(num):
-    if num <= 3:
-        return True
-
-def check_for_stars(hand):
-    for num in hand:
-        if num >3 and num <= 6:
-            return True
-
-def star_cards(n=60, s=3, d=3, hand_size=5):
+def star_cards(n=60, s=3, d=3, draw=5):
     """
-You have a shuffled deck of 60 cards containing the following cards of special
-interest:
-  - Three of the cards in the deck are marked with a diamond.
-  - Three of the cards are marked with a star.  
-  - The remaining cards are nothing special.
-You draw an initial hand of five cards, after which you *must* discard any of
-the star cards for an additional three cards drawn from the top of the deck.
-This process is repeated until you find yourself with a hand that does *not*
-contain any star cards. Write a simulation to approximate the probability that
-your initial draw results in a final hand containing a diamond card.
-"""
-    if hand_size == 0:
+    You have a shuffled deck of 60 cards containing the following cards of special
+    interest:
+    - Three of the cards in the deck are marked with a diamond.
+    - Three of the cards are marked with a star.  
+    - The remaining cards are nothing special.
+    You draw an initial hand of five cards, after which you *must* discard any of
+    the star cards for an additional three cards drawn from the top of the deck.
+    This process is repeated until you find yourself with a hand that does *not*
+    contain any star cards. Write a simulation to approximate the probability that
+    your initial draw results in a final hand containing a diamond card.
+    """
+    hand = np.random.choice(range(1, n+1), draw)
+    if n == 0:
         return 0
-    hand = np.random.choice(range(n), hand_size)
-    n_diamonds = sum(1 for x in hand if x<d)
-    n_stars = sum(1 for x in hand if x>d and x<d+s)
-    if n_stars >= 1:
-        n_diamonds += star_cards(n-hand_size, s-n_stars, d-n_diamonds, 3*n_stars)
-    return int(n_diamonds > 0)
+    stars = 0
+    for c in hand:
+        if c <= d:
+            return 1
+    if (c > d) and (c <= d+s):
+        stars += 1
+    if stars > 0:
+        return star_cards(n - draw, s - stars, d = 3, draw = 3*stars)
+    if stars == 0:
+        return 0
